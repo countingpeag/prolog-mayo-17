763d1c95d3a155b34857150cf498036953b12e30
countingpeag <omar4335@hotmail.com>
2017-05-04 08:23:16 -0500

    operaciones

---
+++ suma/operaciones.pl
@@ -0,0 +1,4 @@
+suma(Sumando1,Sumando2,Resultado):-Resultado is Sumando1 + Sumando2.
+resta(Re1,Re2,Res):-Res is Re1 - Re2.
+divicion(Div1,Div2,Res):-Res is Div1 / Div2.
+multiplicacion(Mul1,Mul2,Res):-Res is Mul1 * Mul2.
